55ce2e597dfb43f79f016f880c24d4b048209622
Rishabh singh <125394319+RishabhSingh0712@users.noreply.github.com>
2026-03-24 16:44:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -202,7 +202,7 @@ document.addEventListener('DOMContentLoaded', () => {
     submitBtn.disabled = true;
 
     try {
-      const response = await fetch("http://127.0.0.1:5000/api/contact", {
+      const response = await fetch("https://portfolio-backend-oxs0.onrender.com/api/contact", {
         method: "POST",
         headers: {
           'Content-Type': 'application/json',
